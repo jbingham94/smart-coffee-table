f738472e9d1b66175a82f46f1f3248e237834724
Jasper K. Bingham <jbingham@cs.dartmouth.edu>
2015-08-06 18:42:33 -0400

    tons of stuff

--- display.py
+++ display.py
@@ -21,23 +21,25 @@ import textwrap
 
 
 def main():
-    
+
+    # make window
     win = GraphWin("Smart Coffee Display",1365,900) 
+
+    # set up background
     background_image="./static/26.gif"
     background(background_image, win)
 
+    # set up main modules
     mail(win)
-
     news(win)
-
     calendar(win)
-
-    weatherTags("WEATHER", win)
     weather(win)
 
+	# create continuous loop to make running clock
     var = 1
-    while var == 1:
-    	obj = time(win)
+    while var == 1: 
+    	obj = clock(win)
+    	time.sleep(1)
     	obj.undraw()
 
     # wait for mouse click to close window
@@ -47,8 +49,9 @@ def main():
     win.close()    # Close window when done
 
 
-def time(win):
+def clock(win):
 
+	# displaying time
 	time = datetime.datetime.now().time()
 	time_str = time.strftime('%l:%M:%S %p')
 	t= Text(Point(668, 50), time_str)
@@ -61,12 +64,13 @@ def time(win):
 
 def news(win):
 
+	# variables
 	font = "arial"
 	effect_str = "bold"
 	color_str = "white"
 	header_color_str = "yellow"
 	header_str = "NEWS"
-	s = 1080
+	s = 1095
 	stor_num = 5
 	big_sz = 11
 	sm_sz = 10
@@ -76,8 +80,8 @@ def news(win):
 	big_incr = 40
 	stories = 3
 	elem = 3
-	header_x = 895
-	header_y = 55
+	header_x = 1095
+	header_y = 50
 	header_sz = 24
 	n = getNews(stor_num)
 
@@ -89,6 +93,8 @@ def news(win):
 	t.setFace(font)
 	t.setTextColor(header_color_str)
 	t.draw(win)
+
+	# stories
 
 	for x in range(0, stories):
 		for y in range(0, elem):
@@ -118,16 +124,17 @@ def mail(win):
 	font = "arial"
 	color_str = "white"
 	emails = 3
-	s = 270 # width for email text
-	header_x = 130
-	header_y = 55
+	x_coord = 270 # width for email text
+	header_x = 270
+	header_y = 50
 	header_size = 24
 	start_coord = 100
 	sm_incr = 20
 	mid_incr = 33
-	big_incr = 37
-	font_sz = 16
+	big_incr = 35
+	font_sz = 14
 	elem = 4
+	subj_slot = 2
 	m = getMail(emails)
 
 	# header stuff
@@ -142,10 +149,12 @@ def mail(win):
 	# displaying emails
 	for x in range(0, emails):
 		for y in range(0, elem):
-			t = Text(Point(s, start_coord), m[x][y])
+			t = Text(Point(x_coord, start_coord), m[x][y])
 			t.setFace(font)
 			t.setSize(font_sz)
 			t.setTextColor(color_str)
+			if y == subj_slot:
+				t.setStyle(header_style)
 			t.draw(win)
 			if y == elem-2: # second to last element in email
 				start_coord = start_coord + big_incr
@@ -158,36 +167,71 @@ def weather(win):
 
 	zipcode = "03755"
 	font = "arial"
-	style_str = "bold italic"
+	style_str = "bold"
 	color_str = "white"
-	size = 28
+	tag_color_str = "yellow"
+	header_str = "WEATHER"
+	size = 20
 	start_x_coord = 170
 	start_y_coord = 500
-	newline_x_coord = 270
-	newline_y_coord = 600
+	x_coord = start_x_coord
+	y_coord = start_y_coord
+	bottom_x_coord = 270
 	x_inc = 200
 	y_inc = 30
+	newline_y_inc = 100
 	days = 5
 	elem = 2
 	w = getWeather(zipcode)
+	style_str = "bold"
+	font = "arial"
+	header_sz = 24
+	day_sz = 16
+	# for header
+	header_x = 270
+	header_y = 450
+	incr = 200
+
+	# header setup
+	point = Point(header_x, header_y)
+	t = Text(point, header_str)
+	t.setSize(header_sz)
+	t.setStyle(style_str)
+	t.setFace(font)
+	t.setTextColor(tag_color_str)
+	t.draw(win)
 
 	for x in range(0, days):
 		for y in range(0, elem):
-			t = Text(Point(start_x_coord, start_y_coord), w[x][y])
+			t = Text(Point(x_coord, y_coord), w[x][y])
 			t.setFace(font)
 			t.setStyle(style_str)
 			t.setSize(size)
 			t.setTextColor(color_str)
 			t.draw(win)
 			if y == elem-1:
-				start_x_coord = start_x_coord + x_inc
-				start_y_coord = start_y_coord - y_inc
+				# adding tag
+				y_coord = y_coord + y_inc
+				date = datetime.date.today() + datetime.timedelta(days=x)
+				point = Point(x_coord, y_coord)
+				t = Text(point, date.strftime('%A'))
+				t.setSize(day_sz)
+				t.setStyle(style_str)
+				t.setFace(font)
+				t.setTextColor(tag_color_str)
+				t.draw(win)	
+				# reset for next weather day
+				x_coord = x_coord + x_inc
+				y_coord = y_coord - y_inc * 2
 			else:
-				start_y_coord = start_y_coord + y_inc
-			if x == 2:
-				start_x_coord = newline_x_coord
-				start_y_coord = newline_y_coord
-
+				y_coord = y_coord + y_inc
+			if x == 1 and y == 1: # after 2 days
+				x_coord = start_x_coord
+				y_coord = y_coord + newline_y_inc
+			if x == 3 and y == 1: # last day goes in middle on bottom
+				x_coord = bottom_x_coord
+				y_coord = y_coord + newline_y_inc
+		
 
 def calendar(win):
 
@@ -200,9 +244,9 @@ def calendar(win):
 	events = 2
 	elem = 2
 	size = 20
-	x_coord = 1030
-	header_x = 925
-	header_y = 470
+	header_x = 1095
+	header_y = 450
+	x_coord = 1095
 	start_coord = 520
 	decr = 25
 	incr = 90
@@ -219,6 +263,8 @@ def calendar(win):
 	t.setFace(font)
 	t.setTextColor(header_color_str)
 	t.draw(win)
+
+	# actual events
 
 	for x in range(0, events):
 		if no_more_events == 1:
@@ -238,42 +284,6 @@ def calendar(win):
 			else:
 				start_coord = start_coord - decr
 	
-
-def weatherTags(tex, win):
-
-	style_str = "bold"
-	font = "arial"
-	color_str = "yellow"
-	days = 5
-	big_sz = 24
-	sm_sz = 16
-	# for header
-	header_x = 130
-	header_y = 400
-	# for the actual days, not the header
-	start_x_coord = 170
-	start_y_coord = 660
-	incr = 200
-
-	# header setup
-	point = Point(header_x, header_y)
-	t = Text(point, tex)
-	t.setSize(big_sz)
-	t.setStyle(style_str)
-	t.setFace(font)
-	t.setTextColor(color_str)
-	t.draw(win)
-
-	for x in range(0, days):
-		date = datetime.date.today() + datetime.timedelta(days=x)
-		point = Point(start_x_coord, start_y_coord)
-		t = Text(point, date.strftime('%A'))
-		t.setSize(sm_sz)
-		t.setStyle(style_str)
-		t.setFace(font)
-		t.setTextColor(color_str)
-		t.draw(win)	
-		start_x_coord = start_x_coord + 200
 
 def background(img_name, win):
 
